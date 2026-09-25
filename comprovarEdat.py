3159fb61fb0a53cbd5d3dfbdb485a577f381eb76
Kevin Quiroz <kevin.quiroz.7ea@itb.cat>
2026-09-25 10:31:57 +0200

    Comprovar l'edat

---
+++ comprovarEdat.py
@@ -0,0 +1,21 @@
+"""
+
+Kevin Quiroz 
+
+25/09/2026
+
+ASIXc Llenguatges de marques
+
+Descripció: Exemple dels apunts
+
+"""
+
+#Programa que demana l'edat a l'usuari i comprova si és major d'edat o no.
+edat=int(input("Introdueix la teva edat: "))
+if edat>=18:
+    print("Ets major d'edat")
+    if edat<18:
+        print("Ets menor d'edat")
+        print("No pots accedir a la pàgina web")
+
+print("Programa finalitzat")
